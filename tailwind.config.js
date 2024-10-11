c496e051ef240c3847b82eed49565549c82e5c3e
Nakayiza Kevina <kevinanakayiza@gmail.com>
2024-10-11 12:23:07 +0300

    this my takewake project and am done with my landing page on a mobile screen

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "darkgreen":"#028C6A",
+        "lightgreen":"7BC5AE",
+        "green":"D1EDE1"
+        
+      },
+
+        fontFamily:{
+          "font-family": ["Roboto", "sans-serif"],
+          "font-weight":"100",
+          "font-style":"normal",
+        }
+        
+      
+       
+      
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
